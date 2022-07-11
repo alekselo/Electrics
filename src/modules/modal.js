4319f73d71eceb55e18cbe84016114125e2ff047
Alexey Loktev <a.a.loktev@yandex.ru>
2022-07-11 13:05:34 +0300

    Сделано открытие и закрытие модальное окна при клики по нужным элементам

---
+++ src/modules/modal.js
@@ -0,0 +1,38 @@
+import { animate } from "./helpers";
+
+const modal = () => {
+  const modal = document.querySelector(".popup");
+  const buttons = document.querySelectorAll(".popup-btn");
+  const modalOverlay = document.querySelector(".modal-overlay");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (document.documentElement.offsetWidth > 768) {
+        modal.style.display = "block";
+        modalOverlay.style.display = "block";
+        animate({
+          duration: 350,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            modal.style.opacity = 100 * progress + "%";
+          },
+        });
+      } else {
+        modal.style.display = "block";
+      }
+    });
+  });
+
+  addEventListener("click", (e) => {
+    if (
+      e.target.closest(".modal-overlay") ||
+      e.target.closest(".modal-close")
+    ) {
+      modal.style.display = "none";
+      modalOverlay.style.display = "none";
+    }
+  });
+};
+export default modal;
